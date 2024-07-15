140e3c9afa47f51eebffd45c62a45e76a2fddb59
edabeyza <edabeyzaozeren@gmail.com>
2024-07-16 00:50:12 +0300

    up

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -2,3 +2,17 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+// Mongoose
+// $ npm i mongoose
+
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+
+const dbConnection = async () => {
+
+    mongoose.connect(process.env?.MONGO_URI || 'mongodb://localhost:27017/blogAPI')
+        .then(() => console.log('* DB Connected.'))
+        .catch(() => console.log('* DB Not Connected.'))
+}
