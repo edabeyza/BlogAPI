604475a1b2851422f83dcd28a982d4d19e6f1fcb
edabeyza <edabeyzaozeren@gmail.com>
2024-07-16 04:49:21 +0300

    up

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,3 +2,17 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+// Call Models:
+const {BlogCategory, BlogPost} = require('../models/blogModel')
+
+/* ------------------------------------------------------- */
+
+// BlogCategory Controller
+
+module.exports.blogCategory = {
+
+    create: async (req, res) => {
+        
+    }
+}
