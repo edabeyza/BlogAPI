c6bc0f1824beace8c90e0dadbe0a8f41b1f06a00
edabeyza <edabeyzaozeren@gmail.com>
2024-07-18 20:28:43 +0300

    up

---
+++ src/controllers/authController.js
@@ -0,0 +1,23 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+/* ------------------------------------------------------ */
+
+const { User } = require('../models/userModel') 
+
+// Auth Controller
+
+module.exports = {
+
+    login: async (req, res) => {
+        
+    },
+
+    logout: async (req, res) => {
+        
+    }
+}
+
+/* ------------------------------------------------------ */
---
+++ src/routes/authRouter.js
@@ -0,0 +1,18 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const router = require('express').Router()
+
+// Call Controller:
+const { auth } = require('../controllers/authController')
+
+/* ------------------------------------------------------- */
+
+// URL: /auth ->
+
+
+
+/* ------------------------------------------------------- */
+module.exports = router
